70543ac2b75b52985ef3bb8c04cf4117e2a09752
144droit <indieohren@gmail.com>
2024-10-01 16:42:27 +0200

    added maths skript

---
+++ maths.py
@@ -0,0 +1,11 @@
+# Definieren der Variablen
+a = 10                                      # Setze a auf 10 (Integer)
+b = 3                                       # Setze b auf 3 (Integer)
+
+# Berechnungen und Ausgabe
+print("Summe:", a + b)                      # Schreibe 'Summe:' und Wert aus a plus b
+print("Differenz:", a - b)                  # Schreibe 'Different:' und Wert aus a minus b
+print("Produkt:", a * b)                    # Schreibe 'Produkt:' und Wert aus a mal b
+print ("Ganzzahliger Quotient:", a // b)    # Schreibe 'Ganzzahliger Quotient:' und Wert aus a geteilt durch b
+print("Rest der Division:", a % b)          # Schreibe 'Rest der Division:' und Wert aus a mod b
+print("a hoch b:", a ** b)                   # Schreibe 'a hoch b:' und Wert aus a hoch b

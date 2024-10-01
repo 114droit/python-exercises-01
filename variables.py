bfb16a19a843ab1654723f2ef41ac20812c22f48
144droit <indieohren@gmail.com>
2024-10-01 16:24:57 +0200

    Added variables-skript

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Variablen definieren
+name = "Marc Droit"                                 # String
+age = 34                                            # Ganzzahl (Integer)
+height = 1.88                                       # Dezimalzahl/Fließkommazahl (Float)
+is_student = True                                   # Boolean -> Wahrheitswerte (True oder False)
+
+# Ausgabe der Variablen
+print("Name:", name)                                # Schreibe 'Name:' und Wert von name
+print("Alter:", age)                                # Schreibe 'Alter:' und Wert von age
+print("Größe:", height, "Meter")                    # Schreibe 'Größe:' und Wert von height und 'Meter'
+print("Ist Student:", is_student)                   # Schreibe 'Ist Student und Wert von is_student
